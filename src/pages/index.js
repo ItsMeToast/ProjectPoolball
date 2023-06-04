ecaf763f1322e481d947d037524a6aef7622ac67
ItsMeToast <ItsMeToastyBread@gmail.com>
2023-06-04 17:14:38 -0400

    Audio Player Advanced
    Added random song system, and disclaimer to trigger music

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,11 +1,18 @@
 
 $(function() {
-    //Audio Player functionality
     const openingSong = $("#song");
     const audioBtn = $("#audio-btn");
 
-    $('#audio-btn').on("click", function() {
+    //Popup
+    const popupBtn = $("#popup-btn");
+    popupBtn.on("click", function() {
+        openingSong.attr("src", `src/public/openingSong${Math.floor(Math.random() * 3) + 1}.mp3`)
         openingSong.get(0).play();
+        $(".popup").hide();
+    });
+
+    //Audio Player functionality
+    audioBtn.on("click", function() {
         openingSong.prop("muted", !(openingSong.prop("muted")));
         openingSong.prop("muted") ? audioBtn.attr("src", "src/public/noaudio.png") : audioBtn.attr("src", "src/public/audio.png");
     });
