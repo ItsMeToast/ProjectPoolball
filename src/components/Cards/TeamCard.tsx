96680b5d8d363dafcd29ac8c8bdab0e0181e05e7
ItsMeToast <ItsMeToastyBread@gmail.com>
2023-07-20 19:55:39 -0400

    Teams Tab Style
    Added formatting to Teams tab, and hover overlay

--- src/components/Cards/TeamCard.tsx
+++ src/components/Cards/TeamCard.tsx
@@ -1,9 +1,19 @@
 import "./TeamCard.css";
-
 function TeamCard(props: any) {
+  const styles = {
+    overlay: {
+      backgroundColor: "var(--" + props.code + "-main-opacity)",
+      color: "var(--" + props.code + "-accent)",
+    },
+  };
+
   return (
     <div className="teamcard container text-center align-items-center px-0 mx-0">
       <img className="teamcard-logo no-interact" src={"../public/" + props.code + ".png"} />
+      <div className="teamcard-overlay text-center position-absolute" style={styles.overlay}>
+        <div className="teamcard-code">{props.code}</div>
+        <div className="teamcard-name">{props.name}</div>
+      </div>
     </div>
   );
 }
--- src/components/Tabs/TeamsTab.tsx
+++ src/components/Tabs/TeamsTab.tsx
@@ -4,20 +4,20 @@ import * as $ from "jquery";
 
 function TeamsTab() {
   const teams = [
-    { code: "ATL", name: "Atlanta Albatross" },
-    { code: "BOS", name: "Boston Revolution" },
-    { code: "CGY", name: "Calgary Forge" },
-    { code: "CHI", name: "Chicago Royalty" },
-    { code: "HAV", name: "Havana Fuego" },
-    { code: "HSN", name: "Houston Stampede" },
-    { code: "LAA", name: "Los Angeles Alliance" },
-    { code: "MBS", name: "Montreal Blue Saints" },
-    { code: "MCC", name: "Mexico City Curse" },
-    { code: "MIA", name: "Miami Gators" },
-    { code: "NOS", name: "New Orleans Surge" },
-    { code: "NYR", name: "New York Rush" },
-    { code: "SEA", name: "Seattle Sharks" },
-    { code: "TOR", name: "Toronto Redcoats" },
+    { id: 1, code: "ATL", name: "Atlanta Albatross" },
+    { id: 2, code: "BOS", name: "Boston Revolution" },
+    { id: 3, code: "CGY", name: "Calgary Forge" },
+    { id: 4, code: "CHI", name: "Chicago Royalty" },
+    { id: 5, code: "HAV", name: "Havana Fuego" },
+    { id: 6, code: "HSN", name: "Houston Stampede" },
+    { id: 7, code: "LAA", name: "Los Angeles Alliance" },
+    { id: 8, code: "MBS", name: "Montreal Blue Saints" },
+    { id: 9, code: "MCC", name: "Mexico City Curse" },
+    { id: 10, code: "MIA", name: "Miami Gators" },
+    { id: 11, code: "NOS", name: "New Orleans Surge" },
+    { id: 12, code: "NYR", name: "New York Rush" },
+    { id: 13, code: "SEA", name: "Seattle Sharks" },
+    { id: 14, code: "TOR", name: "Toronto Redcoats" },
   ];
 
   return (
@@ -25,7 +25,7 @@ function TeamsTab() {
       <div className="teams-display-box container-fluid justify-content-center align-items-center px-4 m-0">
         <div className="row justify-content-center align-items-center w-100">
           {teams.map((team) => (
-            <div className="col g-4 text-center justify-content-center align-self-start">
+            <div key={team.id} className="col gx-4 gy-5 text-center justify-content-center align-self-start">
               <TeamCard code={team.code} name={team.name} />
             </div>
           ))}
